f1a76f79c4f7a0d1cee2950be978b462d31d21fe
Joseph Varghese <josephvarghese.rms@gmail.com>
2024-01-29 09:36:26 -0500

    Added JWT auth token verification from request header

--- helpers/token.js
+++ helpers/token.js
@@ -1,58 +1,50 @@
 const prisma = require("../prismaClient");
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = {
   authenticateUser: async (req, res, next) => {
-    const token = req.get('X-Auth-Token')
-    const email = req.get('X-Auth-Email')
-
+    const token = req.get("X-Auth-Token");
+    const email = req.get("X-Auth-Email");
     let user =
       email &&
-      (await prisma.users.findUnique({
+      (await prisma.user.findUnique({
         where: { email: email },
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          authenticationToken: true,
-          isVerified: true
-        },
-      }))
+      }));
 
     if (
       user != null &&
-      token === user.token &&
+      token === user.authenticationToken &&
       verifyToken(token) &&
-      user.status == 'active'
+      user.isBlocked === false
     ) {
-      req.user = user
-      next()
+      req.user = user;
+      next();
     } else {
       return res.status(401).json({
-        error: 'Could not authenticate with the provided credentials',
-      })
+        error: "Could not authenticate with the provided credentials",
+      });
     }
   },
 
   createToken: (email) => {
     const token = jwt.sign({ email: email }, process.env.JWT_SECRET, {
-      expiresIn: '24h',
-    })
+      expiresIn: "24h",
+    });
 
-    return token
+    return token;
   },
-}
+};
 
 const verifyToken = (token) => {
-  var status
+  var status;
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      status = false
+      status = false;
     } else {
-      status = true
+      status = true;
     }
-  })
+  });
 
-  return status
-}
+  return status;
+};
