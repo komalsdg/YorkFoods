e1a404da4cc73fb8a16e8b0bebb2a63805c95bb4
Joseph Varghese <josephvarghese.rms@gmail.com>
2024-01-29 17:03:14 -0500

    Updated session controller to handle both restaurants and users

--- controllers/api/sessions.js
+++ controllers/api/sessions.js
@@ -3,17 +3,21 @@ const bcrypt = require('bcrypt')
 const prisma = require("../../prismaClient");
 const tokenHelper = require("../../helpers/token");
 
-module.exports.userLogin = async (req, res) => {
-  try{
+module.exports.entityLogin = async (req, res) => {
+  try {
+    const { email, password, type } = req.body; //Pass either "user" or "restaurant"
+    let entity;
 
-    const { email, password } = req.body;
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user && bcrypt.compareSync(password, user.passwordHash)) {
-      const updatedUser = await prisma.user.update({
+    if (type === 'user') {
+      entity = await prisma.user.findUnique({ where: { email } });
+    } else if (type === 'restaurant') {
+      entity = await prisma.restaurant.findUnique({ where: { email } });
+    } else {
+      return res.status(400).json({ error: "Invalid type specified" });
+    }
+
+    if (entity && bcrypt.compareSync(password, entity.passwordHash)) {
+      const updatedEntity = await prisma[type].update({
         where: {
           email,
         },
@@ -21,12 +25,12 @@ module.exports.userLogin = async (req, res) => {
           authenticationToken: tokenHelper.createToken(email),
         },
       });
-      res.json(updatedUser);
+      res.json(updatedEntity);
     } else {
       res.status(401).json({ error: "Invalid email/password" });
     }
-  }catch(error){
-    console.log('Error in session/login', error)
-    res.status(500).send('Something went wrong')
+  } catch (error) {
+    console.log('Error in entity/login', error);
+    res.status(500).send('Something went wrong');
   }
 }
