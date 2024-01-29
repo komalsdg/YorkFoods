941882b6fb4a0f5205d323b09b186633e09a6493
Joseph Varghese <josephvarghese.rms@gmail.com>
2024-01-29 07:41:33 -0500

    Removed usage of passport.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
- const passport = require('passport');
  const router = express.Router();
 
  const users = require('./users');
 
-//  router.use('/users', passport.authenticate('jwt', {session: false}), users);
  router.use('/users', users);
 
  module.exports = router; 
