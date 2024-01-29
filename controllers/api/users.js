bc0971eaa5e633368f655de243707bcc820484f4
Joseph Varghese <josephvarghese.rms@gmail.com>
2024-01-29 09:17:31 -0500

    Sending only required fields in users controller getUsers method

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,6 +1,13 @@
 const prisma = require("../../prismaClient");
 
 module.exports.getUsers = async (req, res) => {
-  const allUsers = await prisma.user.findMany()
+  const allUsers = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      cuisinePreference: true,
+    },
+  });
   res.json(allUsers);
 };
