a1ea1f0d7033b6c68767ef10b13e8503e9ba7233
Joseph Varghese <josephvarghese.rms@gmail.com>
2024-01-30 16:58:50 -0500

    Added new valdation schema for restaurant update

--- validations/restaurant.js
+++ validations/restaurant.js
@@ -1,11 +1,52 @@
-const yup = require('yup');
+const yup = require("yup");
 
 const restaurantSchema = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
-  cuisineType: yup.string().oneOf(['ITALIAN', 'MEXICAN', 'CHINESE', 'JAPANESE', 'INDIAN', 'AMERICAN', 'FRENCH', 'KOREAN', 'BRITISH', 'MEDITERRANEAN', 'CARIBBEAN', 'CONTINENTAL', null]).required(),
+  cuisineType: yup
+    .string()
+    .oneOf([
+      "ITALIAN",
+      "MEXICAN",
+      "CHINESE",
+      "JAPANESE",
+      "INDIAN",
+      "AMERICAN",
+      "FRENCH",
+      "KOREAN",
+      "BRITISH",
+      "MEDITERRANEAN",
+      "CARIBBEAN",
+      "CONTINENTAL",
+      null,
+    ])
+    .required(),
   location: yup.string().email().required(),
-  subdomain: yup.string().email().required()
+  subdomain: yup.string().email().required(),
 });
 
-module.exports = { restaurantSchema};
+const restaurantUpdateSchema = yup.object().shape({
+  name: yup.string(),
+  email: yup.string().email(),
+  cuisineType: yup
+    .string()
+    .oneOf([
+      "ITALIAN",
+      "MEXICAN",
+      "CHINESE",
+      "JAPANESE",
+      "INDIAN",
+      "AMERICAN",
+      "FRENCH",
+      "KOREAN",
+      "BRITISH",
+      "MEDITERRANEAN",
+      "CARIBBEAN",
+      "CONTINENTAL",
+      null,
+    ]),
+  location: yup.string().email(),
+  subdomain: yup.string().email(),
+});
+
+module.exports = { restaurantSchema, restaurantUpdateSchema };
