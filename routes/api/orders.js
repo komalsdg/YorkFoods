80acac96249d9002ed0ddbe8e6ff60dbc9d177c7
Joseph Varghese <josephvarghese.rms@gmail.com>
2024-02-01 11:36:01 -0500

    Minor changes in route due to conflict with restaurant APIs

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,12 +1,27 @@
 const ordersController = require("controllers/api/orders");
 const express = require("express");
 const router = express.Router();
-const tokenHelper = require('../../helpers/token');
+const tokenHelper = require("../../helpers/token");
 
-
-router.get("/users/orders", tokenHelper.authenticateEntity, ordersController.getUserOrders);
-router.get("/restaurants/orders", tokenHelper.authenticateEntity, ordersController.getRestaurantsOrders);
-router.post("/orders", tokenHelper.authenticateEntity, ordersController.insertOrder);
-router.put("/orders/:id", tokenHelper.authenticateEntity, ordersController.updateOrderStatus);
+router.get(
+  "/user/orders",
+  tokenHelper.authenticateEntity,
+  ordersController.getUserOrders
+);
+router.get(
+  "/restaurant/orders",
+  tokenHelper.authenticateEntity,
+  ordersController.getRestaurantsOrders
+);
+router.post(
+  "/orders",
+  tokenHelper.authenticateEntity,
+  ordersController.insertOrder
+);
+router.put(
+  "/orders/:id",
+  tokenHelper.authenticateEntity,
+  ordersController.updateOrderStatus
+);
 
 module.exports = router;
